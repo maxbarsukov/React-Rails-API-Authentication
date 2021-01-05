e51e9a6190a54e9e018f095e334efff9c068cde4
maxbarsukov <maximbarsukov@bk.ru>
2021-01-05 19:04:57 +0300

    static controller added for root path

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origin 'http://localhost:3000'
+    origins 'http://localhost:3000'
     resource '*', headers: :any,
                   methods: %i[get post put patch delete options head],
                   credentials: true
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
     # TODO: here your heroku domain
-    origin 'http://mb-test-authentication.heroku.app'
+    origins 'http://mb-test-authentication.heroku.app'
     resource '*', headers: :any,
                   methods: %i[get post put patch delete options head],
                   credentials: true
